cd2c8cfd53ef89318efafa279590c7c8bcd21536
hardijssimkus <hardijssimkus@grobinasskola.lv>
2024-12-04 14:15:46 +0200

    1 uzd

--- 01/main.py
+++ 01/main.py
@@ -1,11 +1,11 @@
-""" Insert Name """
-name = input("Whats your name: ")
+""" hardijs """ #nekas vienkarš virs komentārs
+name = input("Whats your name: ")  #ievadi savu vārdu 
 
-""" Insert age. Also convert age into an Integer value """
-age = int(input("How old are you: "))
+""" Insert age. Also convert age into an Integer value """ #izdari prasības 
+age = int(input("How old are you: ")) # ievadi vecumu
 
 """ Do the math """
-year = str((2018 - age)+100)
+year = str((2018 - age)+100) #šis rēķina kad tev būs 100 gadi kurā gadā
 
 """ Print the result """
-print(name + " will be 100 years old in the year " + year)
+print(name + " will be 100 years old in the year " + year) #izprintē to gadu kad tev būs 100 gadi
